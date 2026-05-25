d2600491752ac0894470b9062bebe44d03b96090
Aimable01 <aimable.kwizera14@gmail.com>
2026-05-25 15:38:10 +0200

    feat: auth request

---
+++ src/main/java/com/ne/backend/dto/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.ne.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Mobile is required")
+    private String mobile;
+}
